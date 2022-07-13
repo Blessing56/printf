c3ee4db4139872650fd78d8f73a28daa0b0d3c07
Blessing56 <blessingnwalozie@gmail.com>
2022-07-13 14:03:06 +0000

    Task 3

---
+++ print_unsint2oct.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_unsInt - function that prints octal numbers
+ * @arg: argument passed
+ * Return: returns count
+ */
+int print_unsint2oct(va_list arg)
+{
+	int count = 0, i;
+	int *arr;
+	unsigned int n = va_arg(arg, unsigned int);
+	unsigned int tmp = n;
+
+	while (n / 8 != 0)
+	{
+		n/=8;
+		count++;
+	}
+	count++;
+	arr = malloc(count * sizeof(int));
+	if (arr == NULL)
+	{
+		free(arr);
+		return (0);
+	}
+	for (i = 0; i < count; i++)
+	{
+		arr[i] = tmp % 8;
+		tmp /= 8;
+	}
+	for (i = count - 1; i >= 0; i--)
+	{
+		_putchar(arr[i] + '0');
+	}
+	free(arr);
+
+	return (count);
+}
